0f5a6a2b9ade29636db439b1a7c17936d1972c93
khaled <khaled.ghandri@gmail.com>
2017-05-10 00:36:00 +0200

    Auto stash before merge of "mana" and "CustomMyCV/mana"
    pdf creator

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -3,7 +3,13 @@
 namespace AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AppBundle extends Bundle
 {
+
+
+
 }
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,10 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -19,4 +20,32 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/pdf", name="pdfexport")
+     */
+
+
+    public function pdfAction(Request $request){
+        $snappy = $this->get("knp_snappy.pdf");
+        $snappy->setOption("encoding","UTF-8");
+
+        $html = $this->renderView('@FirstUsers/admin/pages/test.html.twig', array(
+            //..Send some data to your view if you need to //
+        ));
+
+        $filename = 'myFirstSnappyPDF';
+
+        return new Response(
+            $snappy->getOutputFromHtml($html),
+            200,
+            array(
+                'Content-Type'          => 'application/pdf',
+                'Content-Disposition'   => 'inline; filename="'.$filename.'.pdf"'
+            )
+        );
+
+
+    }
+
 }
