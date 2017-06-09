d84255765032343c6dd8d21af3de4e42a1b56124
MaNiAnA <bilalmana78@gmail.com>
2017-06-09 12:15:43 +0200

    création crud informations utilisateur

---
+++ src/ResumeBundle/Form/UserDetailsType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ResumeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserDetailsType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nom')->add('prenom')->add('adresse')->add('code_postal')->add('ville')->add('pays')->add('email');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ResumeBundle\Entity\UserDetails'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'resumebundle_userdetails';
+    }
+
+
+}
