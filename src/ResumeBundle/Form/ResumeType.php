d095df9790c3519faf6dfc6c505a19003cb49030
MaNiAnA <bilalmana78@gmail.com>
2017-06-11 16:43:35 +0200

    Ajout menu "Mes CVS"

---
+++ src/ResumeBundle/Form/ResumeType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace ResumeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ResumeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre', TextType::class, array(
+                'label' => 'Titre du CV :'
+            ))
+            ->add('dateCreation',DateType::class , array(
+                'label' => 'Date de création'
+            ))
+            ->add('description', TextareaType::class , array(
+                'label' => 'Description'
+            ));
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ResumeBundle\Entity\Resume'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'resumebundle_resume';
+    }
+
+
+}
